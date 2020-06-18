9fa3ed4f197815de0633c133166fd85f61911bad
Liesel Downes <liesel.downes@icloud.com>
2020-06-18 19:36:11 +0930

    Initial Commit

---
+++ Citation Wizard/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Citation Wizard
+//
+//  Created by Liesel on 18/6/20.
+//  Copyright © 2020 Liesel Downes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
